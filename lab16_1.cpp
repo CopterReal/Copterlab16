41452e0103665c7d396ce254297b60eb2186b199
Copter <copter101x18@gmail.com>
2024-02-05 21:08:08 +0700

    completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,37 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a, int N, int M){
+	cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << setw(4) << *(a + i);
+        if((i + 1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *b, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        b[i] = rand()%101/100.0;
+    }
+}
+
+
+void findRowSum(const double *a, double *sum, int N, int M){
+    for(int i = 0; i < N; i++){
+        sum[i] = 0;
+        for(int j = 0; j < M; j++){
+            sum[i] += *(a + i * M + j);
+        }
+    }
+}
+
+void findColSum(const double *a, double *sum, int N, int M){
+    for(int j = 0; j < M; j++){
+        sum[j] = 0;
+        for(int i = 0; i < N; i++){
+            sum[j] += *(a + i * M + j);
+        }
+    }
+}
